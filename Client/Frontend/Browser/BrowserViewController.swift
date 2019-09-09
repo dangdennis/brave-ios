f35b7949f3691195088dd9b87ad613ef9f0e53a8
Brandon T <bthomas@brave.com>
2019-09-09 15:21:44 -0400

    Move code.

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -3123,6 +3123,8 @@ extension BrowserViewController: PreferencesObserver {
             setupTabs()
             updateTabsBarVisibility()
             updateApplicationShortcuts()
+            tabManager.removeAll()
+            tabManager.resetConfiguration()
         case Preferences.General.alwaysRequestDesktopSite.key:
             tabManager.reset()
             self.tabManager.reloadSelectedTab()
